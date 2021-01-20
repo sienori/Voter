59f9de562e9486eb1323526ca4a8056441ad447e
sienori <iilll7d6f@yahoo.co.jp>
2021-01-20 21:15:11 +0900

    Add footer

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import config from './aws-exports';
 import { BrowserRouter as Router, Route, Switch, Redirect, Link } from "react-router-dom";
 import { styled } from '@material-ui/core/styles';
 import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import { amber } from "@material-ui/core/colors";
+import { amber, grey } from "@material-ui/core/colors";
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
@@ -14,6 +14,8 @@ import Toolbar from '@material-ui/core/Toolbar';
 import HowToVote from '@material-ui/icons/HowToVote';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import TextLink from '@material-ui/core/Link';
+import GitHubIcon from '@material-ui/icons/GitHub';
 import Home from './pages/Home';
 import Post from './pages/Post';
 import Vote from './pages/Vote';
@@ -34,6 +36,25 @@ const Main = styled(Container)({
   paddingTop: "30px",
   maxWidth: 600,
   margin: "auto"
+});
+
+const Footer = styled(Box)({
+  display: "flex",
+  position: "absolute",
+  bottom: "8px",
+  left: 0,
+  right: 0,
+  justifyContent: "center",
+  alignItems: "center",
+  textAlign: "center",
+  color: grey["600"]
+});
+
+const Flex = styled(Box)({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  marginLeft: "12px"
 });
 
 const headerButtonStyle =
@@ -64,6 +85,16 @@ ReactDOM.render(
           <Redirect to="/" />
         </Switch>
       </Main>
+      <Footer>
+        <Typography variant="subtitle2">© 2021 Sienori </Typography>
+        <Flex clone>
+          <TextLink href="https://github.com/sienori/Voter/"
+            target="_blank" rel="noopener" color="inherit">
+            <Box mr="3px" clone><GitHubIcon style={{ fontSize: "16px" }} /></Box>
+            <Typography variant="subtitle2">GitHub</Typography>
+          </TextLink>
+        </Flex>
+      </Footer>
     </ThemeProvider>
   </Router>,
   document.getElementById('root')
